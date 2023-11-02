4eeea483ab5f413bf7f416fc4eb27fa6e0b74472
aleleotta <114069815+aleleotta@users.noreply.github.com>
2023-11-02 11:13:12 +0100

    Add project files.

---
+++ EjerciciosASP_Unidad7/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EjerciciosASP_Unidad7.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        public static DateTime getTime() {
+            DateTime time = DateTime.Now;
+            return time;
+        }
+
+        public static int getHour(DateTime time) {
+            int hour = time.Hour;
+            return hour;
+        }
+
+        public static string welcomeMessage() {
+            string welcome = "Welcome!";
+            DateTime time = getTime();
+            int hour = getHour(time);
+            if (hour < 12)
+            {
+                welcome = "Good Morning!";
+            }
+            else if (hour > 12 && hour < 18)
+            {
+                welcome = "Good Afternoon!";
+            }
+            else {
+                welcome = "Good Evening!";
+            }
+            return welcome;
+        }
+
+    }
+}
